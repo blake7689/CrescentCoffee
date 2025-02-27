c1617d61a3ffc55753d2fdf1f51f518ab2e15ffe
blake7689 <blakec7689@gmail.com>
2025-02-27 04:30:20 +0000

    Added about page migration

---
+++ CrescentCoffee/Migrations/20250227042920_aboutPage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrescentCoffee.Migrations
+{
+    /// <inheritdoc />
+    public partial class aboutPage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
